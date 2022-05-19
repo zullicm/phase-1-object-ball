ccf31cbddc42631644f4d25245fec7d11ae4ddf6
Christopher Marzulli <chriszulli15@gmail.com>
2022-05-19 16:09:40 -0400

    commit tree fc477add6152a5789009363c0f8e5e1ed66e9563

---
+++ src/00-objectball.js
@@ -0,0 +1,197 @@
+function gameObject(){
+    return { home : {
+        teamName : 'Brooklyn Nets',
+        colors : ['Black', 'White'],
+        players : {
+            'Alan Anderson' :{
+                number:0,
+                shoe:16,
+                points:22,
+                rebounds:12,
+                assists:12,
+                steals:3,
+                blocks:1,
+                slamDunks:1
+            }, 
+            'Reggie Evans':{
+                number:30,
+                shoe:14,
+                points:12,
+                rebounds:12,
+                assists:12,
+                steals:12,
+                blocks:12,
+                slamDunks:7
+            },
+            'Brook Lopez':{
+                number:11,
+                shoe:17,
+                points:17,
+                rebounds:19,
+                assists:10,
+                steals:3,
+                blocks:1,
+                slamDunks:15
+            },
+            'Mason Plumlee':{
+                number:1,
+                shoe:19,
+                points:26,
+                rebounds:12,
+                assists:6,
+                steals:3,
+                blocks:8,
+                slamDunks:5
+            },
+            'Jason Terry':{
+                number:31,
+                shoe:15,
+                points:19,
+                rebounds:2,
+                assists:2,
+                steals:4,
+                blocks:11,
+                slamDunks:1
+            }
+        }
+    },
+    away : {
+        teamName : 'Charlotte Hornets',
+        colors : ['Turquoise', 'Purple'],
+        players : {
+            'Jeff Adrien' :{
+                number:4,
+                shoe:18,
+                points:10,
+                rebounds:1,
+                assists:1,
+                steals:2,
+                blocks:7,
+                slamDunks:2
+            }, 
+            'Bismak Biyombo':{
+                number:0,
+                shoe:16,
+                points:12,
+                rebounds:4,
+                assists:7,
+                steals:7,
+                blocks:15,
+                slamDunks:10
+            },
+            'DeSagna Diop':{
+                number:2,
+                shoe:14,
+                points:24,
+                rebounds:12,
+                assists:12,
+                steals:4,
+                blocks:5,
+                slamDunks:5
+            },
+            'Ben Gordon':{
+                number:8,
+                shoe:15,
+                points:33,
+                rebounds:3,
+                assists:2,
+                steals:1,
+                blocks:1,
+                slamDunks:0
+            },
+            'Brendan Haywood':{
+                number:33,
+                shoe:15,
+                points:6,
+                rebounds:12,
+                assists:12,
+                steals:22,
+                blocks:5,
+                slamDunks:12
+            }
+        }
+    }
+}
+}
+
+// function objAssignHome(){
+//     return Object.assign())
+// }
+
+
+function homeTeam(){
+    return gameObject().home
+}
+function awayTeam(){
+    return gameObject().away
+}
+
+function homePlayers(){
+    return homeTeam().players
+}
+
+function awayPlayers(){
+    return awayTeam().players
+}
+
+function players(){
+    return Object.assign({}, homeTeam().players, awayTeam().players )
+}
+
+function numPointsScored(playerInput){
+    return players()[playerInput].points
+}
+
+function shoeSize(playerInput){
+    return players()[playerInput].shoe
+}
+
+function teamColors(){
+    Object.assign({}, homeTeam().colors, awayTeam().colors)
+}
+
+function teamColor(teamName){
+    if(teamName === homeName()){
+        return homeTeam().colors
+    }else if(teamName === awayName()){
+        return awayTeam().colors
+    }
+}
+
+function homeName(){
+    return homeTeam().teamName
+}
+
+function awayName(){
+    return awayTeam().teamName
+}
+
+function teamNames(){
+    return [homeName(), awayName()]
+}
+
+function homePlayersNumbers(){
+    const value = Object.values(homePlayers())
+    const values = [value[0].number, value[1].number, value[2].number, value[3].number, value[4].number]
+    return values
+}
+
+function awayPlayersNumbers(){
+    const value = Object.values(awayPlayers())
+    const values = [value[0].number, value[1].number, value[2].number, value[3].number, value[4].number]
+    return values
+}
+
+function playerNumberByTeam(teamName){
+    if(teamName === homeName()){
+        return homePlayersNumbers()
+    }else if(teamName === awayName()){
+        return awayPlayersNumbers()
+    }
+}
+
+function playerStats(playerName){
+    return players()[playerName]
+}
+
+console.log(playerStats('Brendan Haywood'))
